33c85746d32dd7a2f327801e1f730db2c758335f
pooyankhademi <35997474+pooyankhademi@users.noreply.github.com>
2018-02-13 11:32:50 +0100

    New Delta
    Bistability Regime

--- LL_Equation.m
+++ LL_Equation.m
@@ -5,6 +5,7 @@ close all;
 delta1 = 1;
 delta2 = 2;
 delta3 = 3;
+delta4 = sqrt(3);
 %% Calculate
 Y = linspace(0, 6, 1000);
 %
@@ -13,6 +14,7 @@ Xref = Y;
 X1 = Y.^3 - 2*delta1*Y.^2 + (delta1^2 +1)*Y;
 X2 = Y.^3 - 2*delta2*Y.^2 + (delta2^2 +1)*Y;
 X3 = Y.^3 - 2*delta3*Y.^2 + (delta3^2 +1)*Y;
+X4 = Y.^3 - 2*delta4*Y.^2 + (delta4^2 +1)*Y;
 
 %% Plot 
 figure; hold on; box on;
@@ -20,8 +22,10 @@ plot(Xref, Y, '--k');
 plot(X1, Y, '-b');
 plot(X2, Y, '-r');
 plot(X3, Y, '-g');
+plot(X4, Y, '-c');
 axis([0 6 0 4]);
 xlabel('X');
 ylabel('Y');
 title('Stability analysis');
-legend('Reference', '\Delta = 1', '\Delta = 2', '\Delta = 3');
+legend('Reference', '\Delta = 1', '\Delta = 2', '\Delta = 3', '\Delta = sqrt(3)');
+%% Discretization
